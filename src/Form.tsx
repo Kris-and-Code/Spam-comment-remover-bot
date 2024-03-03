27dd087ed8b8e783ad779b06fcfbe696e1c7ed15
Krishcodeshere <96289582+Krishcodeshere@users.noreply.github.com>
2024-03-03 14:42:20 +0100

    Youtube video Preview

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,55 +1,67 @@
 import React, { useState } from 'react';
 import YouTube from 'react-youtube';
 import './Form.css';
-import ThumbnailGenerator from './YouTube';
 
-interface FormData {
-    youtubeUrl: string;
-  }
-  const getYouTubeVideoId = (url: string): string => {
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    const match = url.match(regExp);
-    return match && match[7].length === 11 ? match[7] : '';
+const getYouTubeVideoId = (url: string): string => {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[7].length === 11 ? match[7] : '';
+};
+
+const Form: React.FC = () => {
+  const [formData, setFormData] = useState<{ youtubeUrl: string }>({
+    youtubeUrl: '',
+  });
+
+  const [videoId, setVideoId] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const videoId = getYouTubeVideoId(formData.youtubeUrl);
+    setVideoId(videoId);
   };
 
-  const Form: React.FC = () => {
-    const [formData, setFormData] = useState<FormData>({
-      youtubeUrl: '',
-    
-    });
-  
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      console.log('Form data:', formData);
-      // Here comes the on submit action  ie, video preview and user authentication
-    };
-  
-    
-  
-    return (
-      <div className='formstyle'>
-        <span className='row'>
-          <div className='col-4'> 
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="youtubeUrl">YouTube URL:</label>
-              <div className='urlstyle'>
-              <div className="App">
-      <h1>YouTube Thumbnail Generator</h1>
-      <ThumbnailGenerator />
+  const opts = {
+    height: '360',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div className="formstyle">
+      <span className="row">
+        <div className="col-4">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="youtubeUrl">YouTube URL:</label>
+            <div className="urlstyle">
+              <input
+                type="text"
+                id="youtubeUrl"
+                className="input-field"
+                value={formData.youtubeUrl}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    youtubeUrl: e.target.value,
+                  })
+                }
+              />
+              <br />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+
+        <div className="col-8">
+          {videoId && (
+            <YouTube videoId={videoId} opts={opts} />
+          )}
+        </div>
+      </span>
     </div>
-                
-                <br />
-                <button type="submit">Submit</button>
-              </div>
-            </form>
-          </div>
-  
-          <div className='col-8'> 
-            Youtube video preview
-          </div>
-        </span>
-      </div>
-    );
-  };
-  
-  export default Form;
+  );
+};
+
+export default Form;
